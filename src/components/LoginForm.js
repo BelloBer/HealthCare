f4cb6e2bd2f9ed42086ea8b5b3e8724716d05558
Liteboho Maseli <litebohomaseli3@gmail.com>
2024-07-17 11:45:04 +0200

    1. changed body theme 2. styled incomplete login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 //src/components/LoginForm.js
 import React, { useState } from 'react';
+import "./LoginForm.css";
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
@@ -11,27 +12,38 @@ const LoginForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Email:</label>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Login</button>
-        </form>
+        <div className='wrapper'>
+        <div className='login-form'>
+            <form onSubmit={handleSubmit}>
+                <div className='input-box'>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className='input-box'>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="remember-forgot">
+                        <label>
+                            <input type="checkbox"/>
+                            Remember me
+                        </label>
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+        </div>
     );
 };
 
